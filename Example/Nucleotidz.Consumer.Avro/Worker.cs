31f804f706212ebc32e32d6660e2b505d712fbaf
ahmar-husain <ahmar.husain@maersk.com>
2023-04-14 18:53:15 +0530

    Remove consumer example

--- Example/Nucleotidz.Consumer.Avro/Worker.cs
+++ Example/Nucleotidz.Consumer.Avro/Worker.cs
@@ -1,4 +1,4 @@
-namespace Consumer.Example
+namespace Nucleotidz.Consumer.Avro
 {
     using com.nucleotidz.employee;
     using com.nucleotidz.employee.key;
@@ -13,7 +13,7 @@ namespace Consumer.Example
         public async Task<IEnumerable<TopicPartitionOffset>> HandleAsync(IEnumerable<ConsumeResult<employeeKey, employeeMessage>> consumeResults, CancellationToken cancellationToken)
         {
             Console.WriteLine(consumeResults.Count().ToString());
-            
+            return consumeResults.Select(cr => cr.TopicPartitionOffset);
             await Task.CompletedTask;
         }
     }
