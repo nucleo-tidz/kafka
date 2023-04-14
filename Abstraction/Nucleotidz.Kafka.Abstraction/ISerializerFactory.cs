6d06887fdef5b036e0caecc72bc91ad2d9adef62
ahmar-husain <ahmar.husain@maersk.com>
2023-04-14 13:12:42 +0530

    Added Factory

---
+++ Abstraction/Nucleotidz.Kafka.Abstraction/ISerializerFactory.cs
@@ -0,0 +1,19 @@
+﻿using Confluent.Kafka;
+using Confluent.SchemaRegistry.Serdes;
+using Confluent.SchemaRegistry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleotidz.Kafka.Abstraction
+{
+    public interface ISerializerFactory
+    {
+        public ISerializer<T> CreateSerializer<T>();
+
+        public IDeserializer<T> CreateDeserializer<T>();
+        
+    }
+}
