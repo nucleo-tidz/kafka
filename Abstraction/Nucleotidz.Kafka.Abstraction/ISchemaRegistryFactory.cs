d5b3b8717c2b320bd503fba04ab80ca952753d6a
ahmar-husain <ahmar.husain@maersk.com>
2023-04-14 12:53:41 +0530

    Added Schema Registry

---
+++ Abstraction/Nucleotidz.Kafka.Abstraction/ISchemaRegistryFactory.cs
@@ -0,0 +1,14 @@
+﻿using Confluent.SchemaRegistry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleotidz.Kafka.Abstraction
+{
+    public interface ISchemaRegistryFactory
+    {
+        CachedSchemaRegistryClient Create();
+    }
+}
---
+++ Abstraction/Nucleotidz.Kafka.Abstraction/Options/SchemaRegistryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace Nucleotidz.Kafka.Abstraction
+{
+    public class SchemaRegistryConfiguration
+    {
+        /// <summary>
+        /// Gets or sets schema registry URL.
+        /// </summary>
+        public string Url { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets schema registry user name.
+        /// </summary>
+        public string? Username { get; set; }
+
+        /// <summary>
+        /// Gets or sets schema registry password.
+        /// </summary>
+        public string? Password { get; set; }
+    }
+}
