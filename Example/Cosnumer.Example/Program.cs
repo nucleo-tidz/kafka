d56e99c91e9b9afa2964ade49c044300e1e01da7
ahmar-husain <ahmar.husain@maersk.com>
2023-04-14 16:05:12 +0530

    Added Example

---
+++ Example/Cosnumer.Example/Program.cs
@@ -0,0 +1,16 @@
+using com.nucleotidz.employee.key;
+using com.nucleotidz.employee;
+using Cosnumer.Example;
+using Newtonsoft.Json.Linq;
+using Nucleotidz.Kafka.Abstraction;
+using Nucleotidz.Kafka.Consumer;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddTransient<IHandler<employeeKey, employeeMessage>, Worker>();
+        services.AddConsumer<employeeKey, employeeMessage>(hostContext.Configuration, "Kafka", "Kafka:SchemaRegistry");
+    })
+    .Build();
+
+await host.RunAsync();
---
+++ Example/Cosnumer.Example/Worker.cs
@@ -0,0 +1,20 @@
+namespace Cosnumer.Example
+{
+    using com.nucleotidz.employee;
+    using com.nucleotidz.employee.key;
+    using Confluent.Kafka;
+    using Nucleotidz.Kafka.Abstraction;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class Worker : IHandler<employeeKey, employeeMessage>
+    {
+        public async Task<IEnumerable<TopicPartitionOffset>> HandleAsync(IEnumerable<ConsumeResult<employeeKey, employeeMessage>> consumeResults, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(consumeResults.Count().ToString());
+            return consumeResults.Select(cr => cr.TopicPartitionOffset);
+            await Task.CompletedTask;
+        }
+    }
+}
