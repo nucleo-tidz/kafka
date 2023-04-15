e6657e4941dc67c5b750f7d98427b5ec7b3172a3
ahmar-husain <ahmar.husain@maersk.com>
2023-04-15 09:35:59 +0530

    Fix DI

--- src/Consumer/Nucleotidz.Kafka.Consumer/DependencyInjection.cs
+++ src/Consumer/Nucleotidz.Kafka.Consumer/DependencyInjection.cs
@@ -10,34 +10,38 @@ namespace Nucleotidz.Kafka.Consumer
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddAvroConsumer<TKey, TValue>(this IServiceCollection services,
-            IConfiguration configuration, string ConsumerConfigurationSection, string SchemaRegistryConfigurationSection)
-             where TKey : class
-             where TValue : class
+        private static IServiceCollection AddAvroConsumer(this IServiceCollection services)
         {
-            services.AddTransient<ISerializerFactory, AvroSerializerFactory>()
-                .AddConsumer<TKey, TValue>(configuration, ConsumerConfigurationSection, SchemaRegistryConfigurationSection); 
+            services.AddTransient<ISerializerFactory, AvroSerializerFactory>();
             return services;
         }
-        public static IServiceCollection AddJsonConsumer<TKey, TValue>(this IServiceCollection services,
-           IConfiguration configuration, string ConsumerConfigurationSection, string SchemaRegistryConfigurationSection)
-            where TKey : class
-            where TValue : class
-        {
-            services.AddTransient<ISerializerFactory, JsonSerializerFactory>()
-                .AddConsumer<TKey, TValue>(configuration, ConsumerConfigurationSection, SchemaRegistryConfigurationSection);
+        private static IServiceCollection AddJsonConsumer(this IServiceCollection services)
 
+        {
+            services.AddTransient<ISerializerFactory, JsonSerializerFactory>();
             return services;
         }
-        private static IServiceCollection AddConsumer<TKey, TValue>(this IServiceCollection services,
-            IConfiguration configuration, string ConsumerConfigurationSection, string SchemaRegistryConfigurationSection)
+        public static IServiceCollection AddConsumer<TKey, TValue>(this IServiceCollection services,
+            IConfiguration configuration, string ConsumerConfigurationSection, string SchemaRegistryConfigurationSection, SerializationScheme serializationScheme)
             where TKey : class
             where TValue : class
         {
             services.Configure<ConsumerConfiguration>(configuration.GetSection(ConsumerConfigurationSection));
             services.Configure<SchemaRegistryConfiguration>(configuration.GetSection(SchemaRegistryConfigurationSection));
             services.AddTransient<ISchemaRegistryFactory, SchemaRegistryFactory>();
-            services.AddTransient<IConsumerFactory<TKey,TValue>, ConsumerFactory<TKey, TValue>>();
+            services.AddTransient<IConsumerFactory<TKey, TValue>, ConsumerFactory<TKey, TValue>>();
+            if (serializationScheme == SerializationScheme.avro)
+            {
+                services.AddAvroConsumer();
+            }
+            else if(serializationScheme == SerializationScheme.json)
+            {
+                services.AddJsonConsumer();
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
             services.AddHostedService<Consumer<TKey, TValue>>();
             return services;
         }
